acd2d177556e799c2685c47f3d303a61e8decd83
Eran Krakovsky <ekrako@gmail.com>
2023-05-09 21:21:00 +0300

    add match to guess

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
+use std::{io, cmp::Ordering};
 
 use rand::Rng;
 
 fn main() {
 
     let secret = rand::thread_rng().gen_range(1..=100);
-
+    println!("The secret number is: {}", secret);
     println!("Guess the number!");
 
     println!("Please input your guess.");
@@ -17,4 +17,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    };
 }
